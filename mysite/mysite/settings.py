f2dbade593ec8467eeb76046e862e4e31b65df5c
matveyplevako <matveyplevako@rambler.ru>
2020-05-11 21:58:12 +0300

    add allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env.get("SECRET_KEY", "SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env.get("DEBUG", False))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [env.get('HOST')]
 
 # Application definition
 
